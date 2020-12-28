fe7e08743374d02a68baeef1f222e3d82ff9110c
Xuwei Li <code@xuwei.li>
2020-12-28 10:08:47 -0800

    [1/2] Rename pwa.js to service-worker.js [2] Update js/scripts.js [2.1] Register service-worker.js instead of pwa.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Progressive Web Application Service Worker
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/pwa.js?v=1').then(function (registration) {
+    navigator.serviceWorker.register('/service-worker.js?v=1').then(function (registration) {
         console.log('ServiceWorker registration successful with scope:', registration.scope);
     }).catch(function (error) {
         console.log('ServiceWorker registration failed:', errror);
