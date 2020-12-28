569a44fef329b95039af627ec7820e69e0e122e3
Xuwei Li <code@xuwei.li>
2020-12-28 12:06:15 -0800

    [1] Upgrade service-worker.js [1.1] Support basic offline functionality

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,36 @@
 // Progressive Web Application Service Worker
 
+const CACHE_NAME = 'xuwei.li:v1';
+
 self.addEventListener('fetch', function (event) {
-    event.respondWith(fetch(event.request));
+    //
+    event.respondWith(caches.match(event.request).then(function (response) {
+        // Cache hit - return response
+        if (response) {
+            return response;
+        }
+        // Fetch through network
+        return fetch(event.request).then(function (response) {
+            // #DEBUG
+            // console.log(event.request);
+            // console.log(response);
+
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME).then(function (cache) {
+                cache.put(event.request, responseToCache);
+            });
+
+            return response;
+        });
+    }));
 });
