4d946d07d27b4e21e3a41d3e2a0258fdfc45679d
Xuwei Li <code@xuwei.li>
2020-12-30 06:00:56 -0800

    [1/8] Rename css/backgroud.css to css/background.css [2] Rename icons/favicon-16x16.png to icons/icon-16x16.png [3] Rename icons/favicon-32x32.png to icons/icon-32x32.png [4] Update background.html [4.1] Change according to renames [5] Update index.html [5.1] Change according to renames [5.2] Add market links block [5.3] Move to /css/main.css?v=12 [5.4] Move to /js/scripts?v=3 [6] Update css/main.css [6.1] Extend height after adding market block [7] Update service-worker.js [7.1] Add install event handler [7.2] Add activate event handler [7.3] Refactor clousure format [7.4] Refactor cache naming [8] Update js/scripts.js [8.1] Move to /service-worker.js?v=2

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 // Progressive Web Application Service Worker
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/service-worker.js?v=1').then(function (registration) {
+    navigator.serviceWorker.register('/service-worker.js?v=2').then(function (registration) {
+        // #DEBUG
         console.log('ServiceWorker registration successful with scope:', registration.scope);
     }).catch(function (error) {
+        // #DEBUG
         console.log('ServiceWorker registration failed:', errror);
     });
 }
--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,64 @@
 // Progressive Web Application Service Worker
 
-const CACHE_NAME = 'xuwei.li:v1';
+const VERSION = 'v2';
 
-self.addEventListener('fetch', function (event) {
+var artifacts = [
+    '/',
+    '/background.html',
+    '/favicon.ico',
+    '/manifest.json',
+    '/css/background.css?v=1',
+    '/css/main.css?v=12',
+    '/images/avatar.jpg',
+    '/js/scripts.js?v=3',
+];
+
+// Clear cache promise
+async function clearCache() {
+    caches.keys().then(cacheNames => {
+        return Promise.all(
+            cacheNames.filter(cacheName => {
+                // Return true if you want to remove this cache,
+                // but remember that caches are shared across
+                // the whole origin
+                return cacheName !== VERSION;
+            }).map(cacheName => {
+                return caches.delete(cacheName);
+            })
+        );
+    })
+};
+
+// 'install' event
+self.addEventListener('install', event => {
+    // #DEBUG
+    console.log('Installing Service Worker ' + VERSION);
+    event.waitUntil(
+        caches.open(VERSION).then(cache => {
+            return cache.addAll(artifacts);
+        })
+    );
+    event.waitUntil(clearCache());
+});
+
+// 'activate' event
+// 1. Clear old caches
+self.addEventListener('activate', event => {
+    // #DEBUG
+    console.log('Activating Service Worker ' + VERSION);
+    event.waitUntil(clearCache());
+});
+
+// 'fetch' event
+self.addEventListener('fetch', event => {
     //
-    event.respondWith(caches.match(event.request).then(function (response) {
+    event.respondWith(caches.match(event.request).then(response => {
         // Cache hit - return response
         if (response) {
             return response;
         }
         // Fetch through network
-        return fetch(event.request).then(function (response) {
+        return fetch(event.request).then(response => {
             // #DEBUG
             // console.log(event.request);
             // console.log(response);
@@ -26,7 +74,7 @@ self.addEventListener('fetch', function (event) {
             // to clone it so we have two streams.
             var responseToCache = response.clone();
 
-            caches.open(CACHE_NAME).then(function (cache) {
+            caches.open(VERSION).then(cache => {
                 cache.put(event.request, responseToCache);
             });
 
